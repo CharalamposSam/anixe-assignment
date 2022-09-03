4f6792abd27ff74dfc56451d63c2093d79124adc
Charalampos <44839448+CharalamposSam@users.noreply.github.com>
2022-09-03 18:42:31 +0300

    Delete anixe-assignment directory

--- anixe-assignment/cypress/e2e/common/common.js
+++
@@ -1,8 +0,0 @@
-import {
-	defineStep,
-} from '@badeball/cypress-cucumber-preprocessor'
-
-defineStep('that user is on the Sign Up page', () => {
-    cy.visit('/')
-    cy.get('img').should('have.attr', 'src', 'logo.png')
-})
